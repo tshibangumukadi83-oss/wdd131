57b8b9cb454d73bed19e4a240e7ec20c84da0cbc
tshibangumukadi83-oss <tshibangumukadi83@gmail.com>
2026-05-20 22:25:30 +0200

    Create place.js

---
+++ place.js
@@ -0,0 +1,25 @@
+const temperature = 8;
+const windSpeed = 10;
+
+function calculateWindChill(temp, speed) {
+    return (
+        13.12 +
+        0.6215 * temp -
+        11.37 * Math.pow(speed, 0.16) +
+        0.3965 * temp * Math.pow(speed, 0.16)
+    ).toFixed(1);
+}
+
+const windChill = document.querySelector("#windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
+
+document.querySelector("#currentyear").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    `Last Modification: ${document.lastModified}`;
